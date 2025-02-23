fe794f9d8f805550caa19dffad57ed78f79d940f
Dom Samphier <dominicsamphier@gmail.com>
2025-02-23 16:10:21 +0000

    chore: try to make tabs"

--- run_length_encoding.rb
+++ run_length_encoding.rb
@@ -10,45 +10,44 @@
 
 # ---
 
-
 def encode(str)
-	return '' if str.empty?
+  return '' if str.empty?
 
-	current = str[0]
-	current_count = 1
-	encoded = ''
+  current = str[0]
+  current_count = 1
+  encoded = ''
 
-	str.each_char.with_index do |char, index|
-		next if index == 0
+  str.each_char.with_index do |char, index|
+    next if index == 0
 
-		if current == char
-			current_count += 1
-		else
-			encoded << "#{current_count}#{current}"
-			current = char
-			current_count = 1
-		end
-		encoded << "#{current_count}#{char}" if index + 1 == str.length
-	end
-	encoded
+    if current == char
+      current_count += 1
+    else
+      encoded << "#{current_count}#{current}"
+      current = char
+      current_count = 1
+    end
+    encoded << "#{current_count}#{char}" if index + 1 == str.length
+  end
+  encoded
 end
 
 def decode(str)
-	return '' if str.empty?
+  return '' if str.empty?
 
-	matches = []
-	new_str = str.gsub(/\d+/) do |match|
-		matches.push(match)
-		''
-	end
-	decoded = ''
-	new_str.each_char.with_index do |char, index|
-		decoded << char * matches[index].to_i
-	end
-	decoded
+  matches = []
+  new_str = str.gsub(/\d+/) do |match|
+    matches.push(match)
+    ''
+  end
+  decoded = ''
+  new_str.each_char.with_index do |char, index|
+    decoded << char * matches[index].to_i
+  end
+  decoded
 end
 
-# test
+# Test
 string_to_encode = 'AAAABBBCCDAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB'
 
 encoded_string = encode(string_to_encode)
